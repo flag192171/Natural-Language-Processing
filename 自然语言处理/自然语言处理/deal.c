3b666c0dcf4af4f3a6f0f52ce1111239c76dc2ae
flag192171 <lzcwc97123@hotmail.com>
2015-12-16 08:19:45 +0800

    deal with num + num =

--- 自然语言处理/自然语言处理/deal.c
+++ 自然语言处理/自然语言处理/deal.c
@@ -1,49 +1,17 @@
 //
-//  main.c
+//  deal.c
 //  自然语言处理
 //
-//  Created by 李子晨 on 15/12/15.
+//  Created by 李子晨 on 15/12/16.
 //  Copyright © 2015年 李子晨. All rights reserved.
 //
 
-#include<stdio.h>
-#include<string.h>
 #include "deal.h"
-/*int input_asg(char Num[6][6]); //输入_赋值(函数)
-int judge(char Num[6][6], int N);
-void output(int number);
+#include <stdio.h>
+#include <string.h>
 char judge_word[][6] = {{"zero"}, {"one"}, {"two"}, {"three"}, {"four"}, {"five"}, {"six"}, {"seven"}, {"eight"}, {"nine"}, {"ten"}};
-*/
-int main()
-{
-    char Num[6][6];
-    int result,N;
-    
-    while(1) {
-        N = input_asg(Num);
-        result = judge(Num, N);
-        if(result != 0) {
-            output(result);
-        }	else {
-            break;
-        }
-    }
-    return 0;
-}
 
-/*int input_asg(char Num[6][6]) {
-    int count;
-    
-    for(count = 0; count < 6; count += 1) {
-        scanf("%s",*(Num + count));
-        if(Num[count][0] == '=')
-            return count;
-    }
-    return 0;
-}
-*/
-
-/*int judge(char Num[6][6], int N) {
+int judge(char Num[6][6], int N) {
     int count_1 = 0;
     int count_2 = 0;
     int num_1 = 0;
@@ -66,14 +34,23 @@ int main()
                 if(num_2 != 0)
                     num_2 *= 10;
                 num_2 += count_2;
-                break;					
-            }			
-        }		
+                break;
+            }
+        }
     }
     return num_1 + num_2;
 }
-*/
-/*void output(int number) {
+int input_asg(char Num[6][6]) {
+    int count;
+    
+    for(count = 0; count < 6; count += 1) {
+        scanf("%s",*(Num + count));
+        if(Num[count][0] == '=')
+            return count;
+    }
+    return 0;
+}
+void output(int number) {
     if((number/100) > 0) {
         printf("%s ", judge_word[number/100]);
     }
@@ -82,4 +59,3 @@ int main()
     }
     printf("%s\n", judge_word[number%10]);
 }
-*/
---
+++ 自然语言处理/自然语言处理/deal.h
@@ -0,0 +1,18 @@
+//
+//  deal.h
+//  自然语言处理
+//
+//  Created by 李子晨 on 15/12/16.
+//  Copyright © 2015年 李子晨. All rights reserved.
+//
+
+#ifndef deal_h
+#define deal_h
+
+//char judge_word[][6] = {{"zero"}, {"one"}, {"two"}, {"three"}, {"four"}, {"five"}, {"six"}, {"seven"}, {"eight"}, {"nine"}, {"ten"}};
+int input_asg(char Num[6][6]); //输入_赋值(函数)
+int judge(char Num[6][6], int N);
+void output(int number);
+
+
+#endif /* deal_h */
